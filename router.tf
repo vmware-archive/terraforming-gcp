7a56b75e814d491235d713fa35bc955698f599e6
Kevin Kelani <kkelani@pivotal.io>
2017-03-14 14:49:31 -0700

    Create isoseg instance group only when isoseg is turned on
    [#139728549]
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

--- router.tf
+++ router.tf
@@ -185,7 +185,7 @@ resource "google_compute_forwarding_rule" "cf-ssh" {
 
 resource "google_compute_instance_group" "isoseglb" {
   // Count based on number of AZs
-  count       = 3
+  count       = "${var.create_isoseg_resources == "1" ? 3 : 0}"
   name        = "${var.env_name}-isoseglb-${element(var.zones, count.index)}"
   description = "terraform generated instance group that is multi-zone for isolation segment load-balancing"
   zone        = "${element(var.zones, count.index)}"
