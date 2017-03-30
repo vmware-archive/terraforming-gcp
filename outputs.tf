e3c2015b8868dd12117461292900ccd1f9884f2b
Raina Masand <rmasand@pivotal.io>
2017-03-30 08:17:14 -0700

    opsman ssh keys consistently named across outputs
    - Still too long but this is a good start

--- outputs.tf
+++ outputs.tf
@@ -150,10 +150,10 @@ output "ert_sql_password" {
   value = "${var.ert_sql_db_password}"
 }
 
-output "ops_manager_private_key" {
+output "ops_manager_ssh_private_key" {
   value = "${tls_private_key.ops-manager.private_key_pem}"
 }
 
-output "ops_manager_public_key" {
+output "ops_manager_ssh_public_key" {
   value = "${format("ubuntu:%s", tls_private_key.ops-manager.public_key_openssh)}"
 }
