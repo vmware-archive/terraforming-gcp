ae424c457cd5cd6dc06b09d47f38680df5be652a
Derwei Chan <dchan@pivotal.io>
2017-07-04 09:45:58 -0400

    Updates google storage bucket count to use a ternary. - This makes it more explicit, instead of relying on an unofficial quirk where booleans are converted to 1 or 0.
    [#146088423]

--- storage.tf
+++ storage.tf
@@ -1,23 +1,23 @@
 resource "google_storage_bucket" "buildpacks" {
   name          = "${var.env_name}-buildpacks"
   force_destroy = true
-  count         = "${var.create_gcs_buckets}"
+  count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "droplets" {
   name          = "${var.env_name}-droplets"
   force_destroy = true
-  count         = "${var.create_gcs_buckets}"
+  count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "packages" {
   name          = "${var.env_name}-packages"
   force_destroy = true
-  count         = "${var.create_gcs_buckets}"
+  count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
 
 resource "google_storage_bucket" "resources" {
   name          = "${var.env_name}-resources"
   force_destroy = true
-  count         = "${var.create_gcs_buckets}"
+  count         = "${var.create_gcs_buckets ? 1 : 0}"
 }
