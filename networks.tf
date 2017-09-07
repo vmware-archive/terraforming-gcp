042f239588aecb07da70af6a8c1020fa9a7f01a4
Emily Casey <ecasey@pivotal.io>
2017-09-07 14:37:03 -0400

    Makes CIDR blocks a terraform variable
    - this allows us to override the default cidr ranges
    
    Signed-off-by: Brenda Chan <brchan@pivotal.io>

--- networks.tf
+++ networks.tf
@@ -4,21 +4,21 @@ resource "google_compute_network" "pcf-network" {
 
 resource "google_compute_subnetwork" "management-subnet" {
   name          = "${var.env_name}-management-subnet"
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = "${var.management_cidr}"
   network       = "${google_compute_network.pcf-network.self_link}"
   region        = "${var.region}"
 }
 
 resource "google_compute_subnetwork" "ert-subnet" {
   name          = "${var.env_name}-ert-subnet"
-  ip_cidr_range = "10.0.4.0/22"
+  ip_cidr_range = "${var.ert_cidr}"
   network       = "${google_compute_network.pcf-network.self_link}"
   region        = "${var.region}"
 }
 
 resource "google_compute_subnetwork" "services-subnet" {
   name          = "${var.env_name}-services-subnet"
-  ip_cidr_range = "10.0.8.0/22"
+  ip_cidr_range = "${var.services_cidr}"
   network       = "${google_compute_network.pcf-network.self_link}"
   region        = "${var.region}"
 }
