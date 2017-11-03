ac893087fa501e71d2d74433f2bb823f45ef74be
Genevieve LEsperance <glesperance@pivotal.io>
2017-11-03 09:31:01 -0700

    Add credhub database
    [#152551131]
    
    Signed-off-by: Rowan Jacobs <rojacobs@pivotal.io>

--- external_database/pas_database.tf
+++ external_database/pas_database.tf
@@ -101,3 +101,11 @@ resource "google_sql_database" "locket" {
 
   count = "${var.count}"
 }
+
+resource "google_sql_database" "credhub" {
+  name       = "credhub"
+  instance   = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.locket"]
+
+  count = "${var.count}"
+}
