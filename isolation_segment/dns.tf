a2838e2b69b0cf33f36d5c2c89877692ff401373
Genevieve LEsperance <glesperance@pivotal.io>
2018-01-17 11:19:22 -0800

    Add a dot to the end of the iso-seg record set.
    [#154413725]

--- isolation_segment/dns.tf
+++ isolation_segment/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "wildcard-iso-dns" {
-  name  = "*.iso.${var.dns_zone_dns_name}"
+  name  = "*.iso.${var.dns_zone_dns_name}."
   type  = "A"
   ttl   = 300
   count = "${var.count}"
