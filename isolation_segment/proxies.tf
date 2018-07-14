e5ad3e70f8099b06a412b3878da0a9e165d98c6b
vchrisb <vchrisb@users.noreply.github.com>
2018-07-14 12:23:01 +0200

    revert Use name for ssl certs instead of name_prefix
    fixes https://github.com/pivotal-cf/terraforming-gcp/issues/76

--- isolation_segment/proxies.tf
+++ isolation_segment/proxies.tf
@@ -23,7 +23,7 @@ resource "google_compute_target_https_proxy" "isoseg_https_lb_proxy" {
 }
 
 resource "google_compute_ssl_certificate" "isoseg_cert" {
-  name        = "${var.env_name}-isoseg-lbcert"
+  name_prefix = "${var.env_name}-isoseg-lbcert-"
   description = "user provided ssl private key / ssl certificate pair"
   certificate = "${length(var.ssl_ca_cert) > 0 ? element(concat(tls_locally_signed_cert.ssl_cert.*.cert_pem, list("")), 0) : var.ssl_cert}"
   private_key = "${length(var.ssl_ca_cert) > 0 ? element(concat(tls_private_key.ssl_private_key.*.private_key_pem, list("")), 0) : var.ssl_private_key}"
--- router.tf
+++ router.tf
@@ -78,7 +78,7 @@ resource "google_compute_target_https_proxy" "https_lb_proxy" {
 }
 
 resource "google_compute_ssl_certificate" "cert" {
-  name        = "${var.env_name}-lbcert"
+  name_prefix = "${var.env_name}-lbcert-"
   description = "user provided ssl private key / ssl certificate pair"
   certificate = "${length(var.ssl_ca_cert) > 0 ? element(concat(tls_locally_signed_cert.ssl_cert.*.cert_pem, list("")), 0) : var.ssl_cert}"
   private_key = "${length(var.ssl_ca_cert) > 0 ? element(concat(tls_private_key.ssl_private_key.*.private_key_pem, list("")), 0) : var.ssl_private_key}"
