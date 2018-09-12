f7301dde67bce10f7f381eccb055e81bd4e19333
Lyle Franklin <lfranklin@pivotal.io>
2018-09-12 11:27:57 -0700

    Iso Seg module now respects 'internetless' flag
    - Adds 'iso_seg_haproxy_static_ip' output
    
    [#160404989]
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- isolation_segment/outputs.tf
+++ isolation_segment/outputs.tf
@@ -15,3 +15,7 @@ output "ssl_private_key" {
 output "domain" {
   value = "${replace(replace(element(concat(google_dns_record_set.wildcard-iso-dns.*.name, list("")), 0), "/^\\*\\./", ""), "/\\.$/", "")}"
 }
+
+output "haproxy_static_ip" {
+  value = "${local.haproxy_static_ip}"
+}
