6457b754cb7589e9f502fc31a48874bbd2c71a6d
Gary Liu <galiu@pivotal.io>
2018-11-28 16:18:48 -0800

    Optionally lock down firewall between Director and IsoSeg
    [#161484461] Update firewall rules to limit ports needed between the BOSH director and Isolation Segment VMs
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- modules/isolation_segment/firewalls.tf
+++ modules/isolation_segment/firewalls.tf
@@ -1,3 +1,4 @@
+/* This firewall allows traffic between VMs in the isolation segment. */
 resource "google_compute_firewall" "isoseg-cf-internal" {
   count = "${var.count * var.with_firewalls}"
 
@@ -21,6 +22,7 @@ resource "google_compute_firewall" "isoseg-cf-internal" {
   target_tags = ["isoseg-${var.env_name}"]
 }
 
+/* This firewall allows traffic on certain ports from cf to the isolation segment. */
 resource "google_compute_firewall" "isoseg-cf-ingress" {
   count = "${var.count * var.with_firewalls}"
 
@@ -37,6 +39,7 @@ resource "google_compute_firewall" "isoseg-cf-ingress" {
   }
 }
 
+/* This firewall denies traffic from cf to the isolation segment on all ports. */
 resource "google_compute_firewall" "isoseg-block-cf-ingress" {
   count = "${var.count * var.with_firewalls}"
 
@@ -61,6 +64,7 @@ resource "google_compute_firewall" "isoseg-block-cf-ingress" {
   }
 }
 
+/* This firewall allows traffic on certain ports from the isolation segment to cf. */
 resource "google_compute_firewall" "cf-isoseg-ingress" {
   count = "${var.count * var.with_firewalls}"
 
@@ -105,6 +109,7 @@ resource "google_compute_firewall" "cf-isoseg-ingress" {
   }
 }
 
+/* This firewall denies traffic from the isolation segment to cf on all ports. */
 resource "google_compute_firewall" "cf-block-isoseg-ingress" {
   count = "${var.count}"
 
@@ -114,6 +119,102 @@ resource "google_compute_firewall" "cf-block-isoseg-ingress" {
   source_tags = ["isoseg-${var.env_name}"]
   target_tags = ["cf-${var.env_name}"]
   priority    = 999
+
+  deny {
+    protocol = "icmp"
+  }
+
+  deny {
+    protocol = "tcp"
+  }
+
+  deny {
+    protocol = "udp"
+  }
+}
+
+/* This firewall allows ssh from the bosh director to the PAS subnet. */
+resource "google_compute_firewall" "bosh-to-isoseg-allow" {
+  count = "${var.count * var.with_firewalls}"
+
+  name    = "${var.env_name}-bosh-to-isoseg-allow"
+  network = "${var.network}"
+
+  direction = "INGRESS"
+
+  source_ranges = ["${var.infrastructure_subnet_cidr}"]
+  target_tags   = ["isoseg-${var.env_name}"]
+  priority      = 998
+
+  allow {
+    protocol = "tcp"
+
+    ports = [
+      "22",
+    ]
+  }
+}
+
+resource "google_compute_firewall" "bosh-to-isoseg-deny" {
+  count = "${var.count * var.with_firewalls}"
+
+  name    = "${var.env_name}-bosh-to-isoseg-deny"
+  network = "${var.network}"
+
+  direction = "INGRESS"
+
+  source_ranges = ["${var.infrastructure_subnet_cidr}"]
+  target_tags   = ["isoseg-${var.env_name}"]
+  priority      = 999
+
+  deny {
+    protocol = "icmp"
+  }
+
+  deny {
+    protocol = "tcp"
+  }
+
+  deny {
+    protocol = "udp"
+  }
+}
+
+/* This firewall allows some ports from PAS subnet to bosh director. */
+resource "google_compute_firewall" "isoseg-to-bosh-allow" {
+  count = "${var.count * var.with_firewalls}"
+
+  name    = "${var.env_name}-isoseg-to-bosh-allow"
+  network = "${var.network}"
+
+  direction = "EGRESS"
+
+  target_tags        = ["isoseg-${var.env_name}"]
+  destination_ranges = ["${var.infrastructure_subnet_cidr}"]
+  priority           = 998
+
+  allow {
+    protocol = "tcp"
+
+    ports = [
+      "4222",
+      "25250",
+      "25777",
+    ]
+  }
+}
+
+resource "google_compute_firewall" "isoseg-to-bosh-deny" {
+  count = "${var.count * var.with_firewalls}"
+
+  name    = "${var.env_name}-isoseg-to-bosh-deny"
+  network = "${var.network}"
+
+  direction = "EGRESS"
+
+  target_tags        = ["isoseg-${var.env_name}"]
+  destination_ranges = ["${var.infrastructure_subnet_cidr}"]
+  priority           = 999
 
   deny {
     protocol = "icmp"
